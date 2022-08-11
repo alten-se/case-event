b9d7ac4e715692b77c0843f8d796cc79bad8ce88
Glenn WS <sondellglenn@gmail.com>
2022-08-11 14:11:24 +0200

    simplify

--- lung_ai/model.py
+++ lung_ai/model.py
@@ -1,95 +1,53 @@
-from keras.utils import np_utils
-from keras.layers import add, Conv2D, Input, BatchNormalization, \
-     TimeDistributed, Embedding, LSTM, GRU, Dense, MaxPooling1D, \
-     Dropout, LeakyReLU, ReLU, Flatten, concatenate, Bidirectional
-from keras.layers.merge import concatenate
-from keras.models import Model, load_model
+from typing import Tuple
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout # "CudNNLSTM" is GPU optimized variant of "LTSM"
+import tensorflow as tf
 
 
-def InstantiateModel(in_):
-    '''
-       Architecture of the Deep Learning Model.
-       Args:
-         in_: input tensor shape
-       Returns: Tensor model
-    '''
-    model_2_1 = GRU(
-        32,
-        return_sequences=True,
-        activation=None,
-        go_backwards=True)(in_)
-    model_2 = LeakyReLU()(model_2_1)
-    model_2 = GRU(
-        128,
-        return_sequences=True,
-        activation=None,
-        go_backwards=True)(model_2)
-    # model_2 = BatchNormalization()(model_2)
-    model_2 = LeakyReLU()(model_2)
+print(tf.config.list_physical_devices())
 
-    model_3 = GRU(
-        64,
-        return_sequences=True,
-        activation=None,
-        go_backwards=True)(in_)
-    model_3 = LeakyReLU()(model_3)
-    model_3 = GRU(
-        128,
-        return_sequences=True,
-        activation=None,
-        go_backwards=True)(model_3)
-    # model_3 = BatchNormalization()(model_3)
-    model_3 = LeakyReLU()(model_3)
+if len(tf.config.list_physical_devices("GPU")) > 0:
+    print("CUDA enbabled GPU detected!")
+    from keras.layers import CuDNNLSTM as LSTM
+else:
+    print("no cuda enbaled gpu, falling back to CPU")
+    from keras.layers import LSTM
 
-    model_add_1 = add([model_3, model_2])
+def rnn_model(shape: Tuple[int, int]) -> Sequential:
+    """_summary_
 
-    model_5 = GRU(
-        128,
-        return_sequences=True,
-        activation=None,
-        go_backwards=True)(model_add_1)
-    model_5 = LeakyReLU()(model_5)
-    model_5 = GRU(
-        32,
-        return_sequences=True,
-        activation=None,
-        go_backwards=True)(model_5)
-    model_5 = LeakyReLU()(model_5)
+    Args:
+        shape (time_steps: int,  features: int): the shape of the input that ai should work with
 
-    model_6 = GRU(
-        64,
-        return_sequences=True,
-        activation=None,
-        go_backwards=True)(model_add_1)
-    model_6 = LeakyReLU()(model_6)
-    model_6 = GRU(
-        32,
-        return_sequences=True,
-        activation=None,
-        go_backwards=True)(model_6)
-    model_6 = LeakyReLU()(model_6)
+    Return:
+        rnn_model: tensor model
+    """
+    model = Sequential()
+    time_steps, features = shape
 
-    model_add_2 = add([model_5, model_6, model_2_1])
+    model.add(LSTM(
+    128,
+    input_shape=(time_steps, features),
+    return_sequences=True))
+    model.add(Dropout(0.2))
 
-    model_7 = Dense(64, activation=None)(model_add_2)
-    model_7 = LeakyReLU()(model_7)
-    model_7 = Dropout(0.2)(model_7)
-    model_7 = Dense(16, activation=None)(model_7)
-    model_7 = LeakyReLU()(model_7)
+    model.add(LSTM(128, return_sequences=True))
+    model.add(Dropout(0.2))
 
-    model_9 = Dense(32, activation=None)(model_add_2)
-    model_9 = LeakyReLU()(model_9)
-    model_9 = Dropout(0.2)(model_9)
-    model_9 = Dense(16, activation=None)(model_9)
-    model_9 = LeakyReLU()(model_9)
+    model.add(LSTM(128, return_sequences=True))
+    model.add(Dropout(0.2))
 
-    model_add_3 = add([model_7, model_9])
+    model.add(LSTM(128, return_sequences=True))
+    model.add(Dropout(0.2))
+    
+    model.add(LSTM(128, return_sequences=False))
+    model.add(Dropout(0.2))
 
-    model_10 = Dense(16, activation=None)(model_add_3)
-    # model_10 = BatchNormalization()(model_10)
-    model_10 = LeakyReLU()(model_10)
-    model_10 = Dropout(0.5)(model_10)
-    # model_7 = MaxPooling1D(pool_size=2)(mode)
-    model_10 = Dense(6, activation="softmax")(model_10)
+    model.add(Dense(32, activation="relu")) 
+    model.add(Dropout(0.2))
 
-    return model_10
+    n_disease_classes = 7
+    model.add(Dense(n_disease_classes, activation="softmax"))
+    
+    return model
--- lung_ai/train.py
+++ lung_ai/train.py
@@ -1,11 +1,5 @@
-from keras import backend as K
-from model import InstantiateModel
-from keras.models import Model
-from keras.optimizer_v2.adamax import Adamax
-from keras.layers import Input
-from keras.callbacks import ModelCheckpoint
-import numpy as np
-
+from model import rnn_model
+import tensorflow as tf
 
 def trainModel(x, y):
     '''
@@ -16,45 +10,36 @@ def trainModel(x, y):
 
         Returns: Save Trained model weights.
     '''
-    K.clear_session()
     # batch_size = X.shape[0]
     # time_steps = X.shape[1]
     # data_dim = X.shape[2]
-    (batch_size, time_steps, data_dim) = x.shape
+    (batch_size, time_steps, features) = x.shape
 
-    Input_Sample = Input(shape=(time_steps, data_dim))
-    Output_ = InstantiateModel(Input_Sample)
-    Model_Enhancer = Model(inputs=Input_Sample, outputs=Output_)
+    model = rnn_model((time_steps, features))
 
-    Model_Enhancer.compile(
-        loss='categorical_crossentropy',
-        metrics=['accuracy'],
-        optimizer=Adamax(),
-        run_eagerly=True
-        )
+    opt = tf.keras.optimizers.Adam(learning_rate=1e-3, decay=1e-5)
 
-    # ES = EarlyStopping(monitor='val_loss', min_delta=0.5, patience=200, verbose=1, mode='auto', baseline=None,
-    #    restore_best_weights=False)
+    model.compile(
+        loss="sparse_categorical_crossentropy",
+        optimizer=opt,
+        metrics=["accuracy"]
+    )
 
-    mc = ModelCheckpoint(
-        'best_model.h5',
-        monitor='val_acc',
-        mode='auto',
-        verbose=0,
-        save_best_only=True)
+    # mc = ModelCheckpoint(
+    #     'best_model.h5',
+    #     monitor='val_acc',
+    #     mode='auto',
+    #     verbose=0,
+    #     save_best_only=True)
 
-    # class_weights = class_weight.compute_sample_weight('balanced',
-    #                                                 np.unique(y[:,0],axis=0),
-    #                                                 y[:,0])
 
-    split_index = 10
+
+    split_index = (batch_size * 1) // 3  
+
     train_set = x[split_index:], y[split_index:]
     validate_set = x[:split_index], y[:split_index]
 
-    ModelHistory = Model_Enhancer.fit(x=train_set[0], y=train_set[1], batch_size=10, epochs=1,
-                                      validation_data=validate_set,
-                                      callbacks=[mc],
-                                      # class_weight=class_weights,
-                                      verbose=1)
+    model.fit(train_set[0], train_set[1], epochs=10, validation_data=validate_set)
 
-    return ModelHistory
+
+    return model
