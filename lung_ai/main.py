5e7469db772b8f25712e569308ade2a86508ada7
Glenn WS <sondellglenn@gmail.com>
2022-08-11 14:12:11 +0200

    clean up

--- lung_ai/main.py
+++ lung_ai/main.py
@@ -4,51 +4,17 @@ import os
 
 #current_dir = os.getcwd()
 #    return current_dir + "\\" + path + "\\" + filename
-my_folder = os.path.pardir(__file__)
+my_folder = os.path.dirname(__file__)
 data_folder = "Data" 
 labels_path = os.path.join(
     my_folder, data_folder, "IBCHI_Challenge_diagnosis_v02.csv"
 )
 wav_path = os.path.join(
-    my_folder, data_folder, "data"
+    my_folder, data_folder, "data"+os.path.sep
 )
 
-X, y = InstantiateAttributes(wav_path, labels_path)
+x, y = InstantiateAttributes(wav_path, labels_path)
 
-print(X.ndim)
-# 1
+print("x shape:", x.shape, x.size)
 
-print(type(X.ndim))
-# <class 'int'>
-
-print(X.ndim)
-# 2
-
-print(X.ndim)
-# 3
-
-print(X.shape)
-# (3,)
-
-print(type(X.shape))
-# <class 'tuple'>
-
-print(X.shape)
-# (3, 4)
-
-print(X.shape)
-# (2, 3, 4)
-
-print(X.size)
-# 3
-
-print(type(X.size))
-# <class 'int'>
-
-print(X.size)
-# 12
-
-print(X.size)
-# 24
-
-trainModel(X, y)
+model_history = trainModel(x, y)
