6f41eb870320d886ccb2e7065c1cfcc8f2e3ed42
Glenn WS <sondellglenn@gmail.com>
2022-08-15 10:11:19 +0200

    class based split on test/validate

--- lung_ai/train.py
+++ lung_ai/train.py
@@ -1,8 +1,9 @@
-from model import rnn_model
-import tensorflow as tf
+from typing import Tuple
 from numpy import ndarray
-from keras.models import Sequential
 
+import numpy as np
+import tensorflow as tf
+from keras.models import Sequential
 
 def train(x: ndarray, y:ndarray, model: Sequential) -> Sequential:
     '''
@@ -24,12 +25,46 @@ def train(x: ndarray, y:ndarray, model: Sequential) -> Sequential:
     )
 
     # TODO k-fold cross validation, train diffrent instaseces using diffrent selections for train/validate set
-    split_index = (batch_size * 1) // 3  # use a 1/3 of data for validation
+    # split_index = (batch_size * 1) // 3  # use a 1/3 of data for validation
 
-    train_set = x[split_index:], y[split_index:]
-    validate_set = x[:split_index], y[:split_index]
+    train_set, validate_set = split_data(x, y)
 
-    model.fit(train_set[0], train_set[1], epochs=300, validation_data=validate_set)
+    # train_set = x[split_index:], y[split_index:]
+    # validate_set = x[:split_index], y[:split_index]
+
+    model.fit(train_set[0], train_set[1], epochs=500, validation_data=validate_set)
 
 
     return model
+
+DataSet = Tuple[ndarray, ndarray]
+
+def split_data(data: ndarray, labels: ndarray, fraction = 0.3) -> Tuple[DataSet, DataSet]:
+    """ Splits data set into training and validations sets.
+
+    Ensures the same fraction is used for each class.
+
+    Args:
+        data (ndarray) shape: [batch_size, time_steps, features]
+        labels (ndarray) : [batch_size, ]
+
+    Returns:
+        training and validation sets (Tuple[DataSet, DataSet]): [training_set, validation_set]
+    """
+    validate_mask = np.zeros(labels.shape, dtype=bool)
+    classes = np.unique(labels)
+
+    for c in classes:
+        inds = np.nonzero(c == labels)[0]
+        count_class = len(inds)
+        validate_len = int(np.ceil((count_class*fraction)))
+        validate_inds = inds[:validate_len]
+        validate_mask[validate_inds] = True
+
+    validate_data = data[validate_mask]
+    validate_labels = labels[validate_mask] 
+
+    train_data = data[~validate_mask]
+    train_labels = labels[~validate_mask]
+
+    return (train_data, train_labels), (validate_data, validate_labels)
