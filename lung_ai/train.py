80a5b28d0ade71ba99b26896ea98eff55e95b30c
Glenn WS <sondellglenn@gmail.com>
2022-08-16 09:25:11 +0200

    handle data with varing time length

--- lung_ai/train.py
+++ lung_ai/train.py
@@ -5,6 +5,11 @@ import numpy as np
 import tensorflow as tf
 from keras.models import Sequential
 
+from data_fmt import DataGenerator
+
+DataSet = Tuple[ndarray, ndarray]
+
+
 def train(x: ndarray, y:ndarray, model: Sequential) -> Sequential:
     '''
         Training the Neural Network model against the data.
@@ -14,29 +19,35 @@ def train(x: ndarray, y:ndarray, model: Sequential) -> Sequential:
 
         Returns: Save Trained model weights.
     '''
-    batch_size = x.shape[0]
+    opt = tf.keras.optimizers.Adam(learning_rate=1e-3, decay=1e-3)
 
-    opt = tf.keras.optimizers.Adam(learning_rate=1e-3, decay=1e-5)
+    def get_lr_metric(optimizer):
+        def lr(y_true, y_pred):
+            return optimizer._decayed_lr(tf.float32) # I use ._decayed_lr method instead of .lr
+        return lr
+
+    lr_metric = get_lr_metric(opt)
 
     model.compile(
         loss="sparse_categorical_crossentropy",
         optimizer=opt,
-        metrics=["accuracy"]
+        metrics=["accuracy", lr_metric]
     )
 
     # TODO k-fold cross validation, train diffrent instaseces using diffrent selections for train/validate set
-
     train_set, validate_set = split_data(x, y)
+    training_gen = DataGenerator(train_set[0], train_set[1], batch_size=32, shuffle=True)
+    valid_gen = DataGenerator(validate_set[0], validate_set[1], batch_size=32, shuffle=True)
+
 
     for patient_class in np.unique(validate_set[1]):
         print("cv:", sum(validate_set[1]==patient_class), "ct:", sum(train_set[1]==patient_class), "frac:", sum(validate_set[1]==patient_class)/(sum(validate_set[1]==patient_class) + sum(train_set[1]==patient_class)))
+    print("train_len: ", len(train_set[1]))
+    print("validate_len: ", len(validate_set[1]))
 
-    model.fit(train_set[0], train_set[1], epochs=500, validation_data=validate_set)
-
-
+    model.fit(training_gen, validation_data=valid_gen, epochs=200)
     return model
 
-DataSet = Tuple[ndarray, ndarray]
 
 def split_data(data: ndarray, labels: ndarray, fraction = 0.3) -> Tuple[DataSet, DataSet]:
     """ Splits data set into training and validations sets.
