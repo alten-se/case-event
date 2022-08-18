8ec891d67f51e70c41064a56c9af2ff6e30ddbda
Glenn WS <sondellglenn@gmail.com>
2022-08-18 13:00:04 +0200

    test eval on file

--- lung_ai/tests/test_load_model.py
+++ lung_ai/tests/test_load_model.py
@@ -1,25 +1,53 @@
 import numpy as np
 from os.path import join as p_join
 
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import tensorflow as tf
+tf.get_logger().setLevel('ERROR')
+
 from lungai.load_model import load_trained_model
 from lungai.data_extraction import get_data
-from lungai.paths import TRAINED_MODELS_PATH
-from lungai.evaluate import eval
+from lungai.paths import TRAINED_MODELS_PATH, DATA_PATH
+from lungai.evaluate import eval, eval_sound
+from lungai.clear import clear
 
+
+
+def get_model():
+    return load_trained_model(p_join(TRAINED_MODELS_PATH, "dummy"))
+
+def pretty_print(label: str, confidence: float):
+    print("Prediction: {label}, prob. = {confidence:.3%}".format(
+            label=label, 
+            confidence=confidence))
 
 def test_load():
     data, labels, label_dict = get_data()
-    model = load_trained_model(p_join(TRAINED_MODELS_PATH, "dummy"))
+    model = get_model()
 
     test_inds = [0, 1]
 
     predictions = [ eval(data_point, model, label_dict) for data_point in data[test_inds]]
     
     for label, confidence in predictions:
-        print("Prediction: {label}, prob. = {confidence:.3%}".format(
-            label=label, 
-            confidence=confidence))
+        pretty_print(label, confidence)
+        
     
 
+def test_eval_sound():
+    _, _, label_dict = get_data()
+    model = get_model()
+    record_name = "101_1b1_Al_sc_Meditron.wav"
+    label, conf = eval_sound(
+        p_join(DATA_PATH, record_name),
+        model,
+        label_dict
+        )
+    pretty_print(label, conf)
+
+
+
+
 if __name__ == "__main__":
-    test_load()
+    test_eval_sound()
