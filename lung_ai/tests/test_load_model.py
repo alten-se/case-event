5c90cb6806385e882ce4e9734ebc7e53bfaa63fa
Glenn WS <sondellglenn@gmail.com>
2022-08-18 11:06:33 +0200

    model test help

--- lung_ai/tests/test_load_model.py
+++ lung_ai/tests/test_load_model.py
@@ -1,35 +1,25 @@
-from lungai.model import rnn_model
-from lungai.data_extraction import get_data
-
-import os 
-from keras.utils import pad_sequences
 import numpy as np
+from os.path import join as p_join
 
+from lungai.load_model import load_trained_model
+from lungai.data_extraction import get_data
+from lungai.paths import TRAINED_MODELS_PATH
+from lungai.evaluate import eval
 
-TESTS_PATH = os.path.dirname(__file__)
-PACKAGE_ROOT = os.path.dirname(TESTS_PATH) 
-TRAINED_AI_PATH = os.path.join(PACKAGE_ROOT, "trained_models")
 
 def test_load():
     data, labels, label_dict = get_data()
-    data_shape = (None, data[0].shape[-1])
-    model = rnn_model(data_shape, len(label_dict))
-    model.load_weights(os.path.join(TRAINED_AI_PATH, "dummy/w"))
+    model = load_trained_model(p_join(TRAINED_MODELS_PATH, "dummy"))
 
     test_inds = [0, 1]
 
-    predictions = model.predict(pad_sequences(data[test_inds]))
-    inv_map = {v: k for k, v in label_dict.items()}
+    predictions = [ eval(data_point, model, label_dict) for data_point in data[test_inds]]
     
-    for p in predictions:
-        index = np.argmax(p)
-        print("Prediction: {condition}, prob. = {val:.3%}%".format(
-            condition=inv_map[index], 
-            val=p[index]))
+    for label, confidence in predictions:
+        print("Prediction: {label}, prob. = {confidence:.3%}".format(
+            label=label, 
+            confidence=confidence))
     
-    for index in test_inds:
-        print("Truth: {}".format(inv_map[labels[index]]))
-
 
 if __name__ == "__main__":
     test_load()
