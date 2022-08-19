ff2ec37e708b93e0966e624025324f771e09055d
Glenn WS <sondellglenn@gmail.com>
2022-08-19 13:44:07 +0200

    better names

--- lung_ai/tests/test_ai.py
+++ lung_ai/tests/test_ai.py
@@ -1,5 +1,4 @@
-
-import numpy as np
+import pytest
 from os.path import join
 
 from lungai.tf_config import silence_tf
@@ -11,41 +10,46 @@ from lungai.ai import AI
 
 dummy_path = join(TRAINED_MODELS_PATH, "dummy")
 
+@pytest.fixture
+def dummy():
+    return AI.load(dummy_path)
+
+@pytest.fixture
+def data_set():
+    return get_data()
+
+
 def pretty_print(label: str, confidence: float):
     print("Prediction: {label}, prob. = {confidence:.3%}".format(
             label=label, 
             confidence=confidence))
 
+def test_load(dummy: AI):
+    ai = dummy
+    ai.model.summary()
+    print(ai.label_dict)
+    print(ai.io_shape)
 
-def test_load(silent=False):
-    ai = AI.load(dummy_path)
-    if not silent:
-        ai.model.summary()
-        print(ai.label_dict)
-        print(ai.io_shape)
 
-    return ai
-
-def test_predict():
-    ai = test_load(silent=True)
-
-    data = get_data()[0]
-
+def test_predict(dummy: AI, data_set):
+    data = data_set[0]
     test_inds = [0, 1]
-
-    predictions = [ ai.predict_one(item) for item in data[test_inds]]
+    predictions = [ dummy.predict_one(item) for item in data[test_inds]]
     
     for label, confidence in predictions:
         pretty_print(label, confidence)
         
-    
 
-def test_eval_sound():
-    ai = test_load(silent=True)
+def test_eval_sound(dummy: AI):
     record_name = "101_1b1_Al_sc_Meditron.wav"
     record_path = join(DATA_PATH, record_name)
-    label, conf = ai.predict_sound(record_path)
+    label, conf = dummy.predict_sound(record_path)
     pretty_print(label, conf)
 
+def test_train(dummy):
+    pass
+    
+
 if __name__ == "__main__":
+
     test_eval_sound()
